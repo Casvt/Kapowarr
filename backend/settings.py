b0f4fcd01e02834a98187e097aef691db66ff2aa
CasVT <casvantijn@gmail.com>
2023-05-18 00:25:16 +0200

    Version bump

--- backend/settings.py
+++ backend/settings.py
@@ -36,7 +36,7 @@ private_settings = {
 	'comicvine_api_url': 'https://comicvine.gamespot.com/api',
 	'getcomics_url': 'https://getcomics.org',
 	'hosting_threads': 10,
-	'version': 'v1.0.0-beta-19a',
+	'version': 'v1.0.0-beta-19b',
 	'python_version': ".".join(str(i) for i in list(version_info))
 }
 
