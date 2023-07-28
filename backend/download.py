0ecaa92c050c57a99e42e520ed7356d7ebbc7d97
CasVT <casvantijn@gmail.com>
2023-07-28 17:36:23 +0200

    Fixed downloads having a year past release year (#82)

--- backend/download.py
+++ backend/download.py
@@ -467,12 +467,13 @@ def _process_extracted_get_comics_links(
 	volume_number: int
 	volume_year: int
 	issue_count: int
-	volume_title, volume_number, volume_year, issue_count = get_db('dict').execute("""
+	volume_title, volume_number, volume_year, issue_count, last_issue_date = get_db('dict').execute("""
 		SELECT
 			v.title,
 			volume_number,
 			year,
-			COUNT(1) AS issue_count
+			COUNT(1) AS issue_count,
+			MAX(i.date) AS last_issue_date
 		FROM volumes v
 		INNER JOIN issues i
 		ON v.id = i.volume_id
@@ -482,6 +483,7 @@ def _process_extracted_get_comics_links(
 		(volume_id,)
 	).fetchone()
 	tpb_release = issue_count == 1
+	last_year: int = int(last_issue_date.split('-')[0])
 	annual = 'annual' in volume_title.lower()
 	service_preference_order = dict((v, k) for k, v in enumerate(Settings().get_service_preference()))
 
@@ -496,7 +498,7 @@ def _process_extracted_get_comics_links(
 			processed_desc['volume_number'] == volume_year)
 		and (processed_desc['year'] is None
 			or
-			processed_desc['year'] == volume_year)
+			volume_year <= processed_desc['year'] <= last_year)
 		and (tpb_release == (processed_desc['special_version'] == 'tpb')
 			or
 			processed_desc['issue_number'])
