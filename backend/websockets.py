b0edb6582d8cec27f3d8bed6f074d339bf9a1cac
Aaron Eikenberry <email@aaroneikenberry.com>
2024-01-10 19:00:00 -0600

    fixes

--- backend/websockets.py
+++ backend/websockets.py
@@ -21,15 +21,19 @@ class EventServer:
         self.thread.start()
 
     def stop(self):
-        self.server.close()
+        self.server.shutdown_gracefully()
         self.thread.stop()
 
     def _new_client(self, client, server):
         print("New client connected and was given id %d" % client["id"])
 
+    def _receive(self, client, server, message):
+        print(f"Received message from client {client['id']}: {message}")
+
     def _start_server(self):
         self.server = WebsocketServer(host="", port=8000)
         self.server.set_fn_new_client(self._new_client)
+        self.server.set_fn_message_received(self._receive)
         self.server.run_forever()
 
 
