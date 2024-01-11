6ca9c923be1e92b0087b12b522bcd0d63fb03e7e
Aaron Eikenberry <email@aaroneikenberry.com>
2024-01-10 18:11:32 -0600

    cleanup

--- Kapowarr.py
+++ Kapowarr.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 import logging
 
@@ -8,47 +8,46 @@ from backend.files import folder_path
 from backend.helpers import check_python_version
 from backend.logging import setup_logging
 from backend.server import create_app, create_waitress_server, set_url_base
-from backend.websockets import EventServer
 from frontend.api import download_handler, settings, task_handler, event_server
 
 
 def Kapowarr() -> None:
-	"""The main function of Kapowarr
-	"""
-	setup_logging()
-	logging.info('Starting up Kapowarr')
+    """The main function of Kapowarr"""
+    setup_logging()
+    logging.info("Starting up Kapowarr")
 
-	if not check_python_version():
-		exit(1)
+    if not check_python_version():
+        exit(1)
 
-	set_db_location(folder_path(*__DATABASE_FILEPATH__))
+    set_db_location(folder_path(*__DATABASE_FILEPATH__))
 
-	app = create_app()
+    app = create_app()
 
-	with app.app_context():
-		setup_db()
+    with app.app_context():
+        setup_db()
 
-		url_base = settings.get_settings()['url_base']
-		set_url_base(app, url_base)
+        url_base = settings.get_settings()["url_base"]
+        set_url_base(app, url_base)
 
-		download_handler.create_download_folder()
+        download_handler.create_download_folder()
 
-	download_handler.load_download_thread.start()
-	task_handler.handle_intervals()
+    download_handler.load_download_thread.start()
+    task_handler.handle_intervals()
 
-	host: str = settings.cache['host']
-	port: int = settings.cache['port']
-	server = create_waitress_server(app, host, port)
-	logging.info(f'Kapowarr running on http://{host}:{port}{url_base}/')
-	# =================
-	server.run()
-	# =================
+    host: str = settings.cache["host"]
+    port: int = settings.cache["port"]
+    server = create_waitress_server(app, host, port)
+    logging.info(f"Kapowarr running on http://{host}:{port}{url_base}/")
+    # =================
+    server.run()
+    # =================
 
-	download_handler.stop_handle()
-	task_handler.stop_handle()
-	event_server.stop()
+    download_handler.stop_handle()
+    task_handler.stop_handle()
+    event_server.stop()
 
-	return
+    return
+
 
 if __name__ == "__main__":
-	Kapowarr()
+    Kapowarr()
--- backend/websockets.py
+++ backend/websockets.py
@@ -1,40 +1,43 @@
 from threading import Event, Thread
 
-from simple_websocket_server import WebSocketServer, WebSocket
+from websocket_server import WebsocketServer
+
 
 class EventServer:
-
     def __init__(self) -> None:
         self.thread = None
         self.server = None
 
     def publish(self, data):
-        pass
+        self.server.send_message_to_all(data)
 
     def run(self):
         self.thread = StoppableThread(
             target=self._start_server,
             args=(),
             name="Websocket Server Thread",
-            daemon=True
+            daemon=True,
         )
         self.thread.start()
 
     def stop(self):
+        self.server.close()
         self.thread.stop()
 
-    def _start_server(self):
-        self.server = WebSocketServer('', 8000, SimpleEcho)
-        print("serving")
-        self.server.serve_forever()
+    def _new_client(self, client, server):
+        print("New client connected and was given id %d" % client["id"])
 
+    def _start_server(self):
+        self.server = WebsocketServer(host="", port=8000)
+        self.server.set_fn_new_client(self._new_client)
+        self.server.run_forever()
 
 
 class StoppableThread(Thread):
     """Thread class with a stop() method. The thread itself has to check
     regularly for the stopped() condition."""
 
-    def __init__(self,  *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super(StoppableThread, self).__init__(*args, **kwargs)
         self._stop_event = Event()
 
@@ -43,15 +46,3 @@ class StoppableThread(Thread):
 
     def stopped(self):
         return self._stop_event.is_set()
-
-
-class SimpleEcho(WebSocket):
-    def handle(self):
-        # echo message back to client
-        self.send_message(self.data)
-
-    def connected(self):
-        print(self.address, 'connected')
-
-    def handle_close(self):
-        print(self.address, 'closed')
