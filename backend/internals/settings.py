60476e6773708e1a63a6ac07bbde765227cfc403
CasVT <casvantijn@gmail.com>
2025-01-22 14:38:41 +0100

    Set version to alpha-35

--- backend/internals/settings.py
+++ backend/internals/settings.py
@@ -72,7 +72,7 @@ class SettingsValues:
 
 
 about_data = {
-    'version': 'alpha-34',
+    'version': 'alpha-35',
     'python_version': get_python_version(),
     'database_version': get_latest_db_version(),
     'database_location': None, # Get's filled in by db.set_db_location()
