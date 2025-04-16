d6eb4d6c26881d188e0643135b17e2908e5e3478
CasVT <casvantijn@gmail.com>
2025-04-16 16:46:29 +0200

    Set version to v1.2.0

--- backend/internals/settings.py
+++ backend/internals/settings.py
@@ -74,7 +74,7 @@ class SettingsValues:
 
 
 about_data = {
-    'version': 'alpha-36',
+    'version': 'v1.2.0',
     'python_version': get_python_version(),
     'database_version': get_latest_db_version(),
     'database_location': None, # Get's filled in by db.set_db_location()
