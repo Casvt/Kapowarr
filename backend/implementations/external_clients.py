5cf13201e372e1245ac6e3823a27b1877eeedbd8
CasVT <casvantijn@gmail.com>
2026-04-22 21:39:06 +0200

    Automatically detect external clients to import
    We used to have to add the external client implementations to the import statement in the external_clients.py file so that it would pop up in the list. Now it just dynamically finds the clients in the folder and imports them all.

--- backend/implementations/external_clients.py
+++ backend/implementations/external_clients.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 
+from functools import lru_cache
+from importlib import import_module
+from os.path import basename, dirname, splitext
 from sqlite3 import IntegrityError
 from typing import Any, Dict, List, Mapping, Type, Union
 
+import backend.implementations.torrent_clients as tc
 from backend.base.custom_exceptions import (ClientNotWorking,
                                             CredentialInvalid,
                                             ExternalClientDownloading,
@@ -10,6 +14,7 @@ from backend.base.custom_exceptions import (ClientNotWorking,
                                             InvalidKeyValue, KeyNotFound)
 from backend.base.definitions import (ClientTestResult, DownloadType,
                                       ExternalDownloadClient)
+from backend.base.files import list_files
 from backend.base.helpers import get_subclasses, normalise_base_url
 from backend.internals.db import get_db
 
@@ -158,14 +163,20 @@ class BaseExternalClient(ExternalDownloadClient):
 # =====================
 class ExternalClients:
     @staticmethod
+    @lru_cache(1)
     def get_client_types() -> Dict[str, Type[ExternalDownloadClient]]:
         """Get a mapping of the client type strings to their class.
 
         Returns:
             Dict[str, Type[ExternalDownloadClient]]: The mapping.
         """
-        from backend.implementations.torrent_clients import (Transmission,
-                                                             qBittorrent)
+        for file in list_files(
+            dirname(tc.__file__ or '')
+        ):
+            if file.endswith(".py") and not file.endswith("__init__.py"):
+                module_name = splitext(basename(file))[0]
+                import_module(f"{tc.__name__}.{module_name}")
+
         return {
             client.client_type: client
             for client in sorted(
