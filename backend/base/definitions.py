60a53307aa3ee8c9f55176f1a0a82d780be8751a
CasVT <casvantijn@gmail.com>
2026-04-25 22:54:08 +0200

    Increase interval of updating torrent statuses from 5s to 30s

--- backend/base/definitions.py
+++ backend/base/definitions.py
@@ -9,8 +9,8 @@ from abc import ABC, abstractmethod
 from dataclasses import asdict, dataclass
 from enum import Enum
 from threading import Event, Thread
-from typing import (TYPE_CHECKING, Any, Callable, Dict, List, Mapping,
-                    Sequence, Tuple, TypedDict, TypeVar, Union)
+from typing import (TYPE_CHECKING, Any, Callable, Dict, List,
+                    Mapping, Tuple, TypedDict, TypeVar, Union)
 
 if TYPE_CHECKING:
     from threading import Timer
@@ -132,7 +132,7 @@ class Constants:
     when a challenge is presented
     """
 
-    TORRENT_UPDATE_INTERVAL = 5 # seconds
+    TORRENT_UPDATE_INTERVAL = 30 # seconds
     "The interval in seconds between status updates from external clients"
 
     TORRENT_TAG = "kapowarr"
