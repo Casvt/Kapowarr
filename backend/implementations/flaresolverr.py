e31292b91c55b6d3ab78d6e45d584dbdef2fc3c1
CasVT <casvantijn@gmail.com>
2026-07-20 18:31:36 +0200

    Removed half-support for Byparr
    Byparr claimed it is a full drop-in replacement of FS, but it isn't so remove support for it in the FS client

--- backend/implementations/flaresolverr.py
+++ backend/implementations/flaresolverr.py
@@ -87,12 +87,7 @@ class FlareSolverr:
                     return False
 
                 result = result.json()
-                if not (
-                    result.get("status") == "ok"
-                    # Byparr doesn't return the same, even though it states it's
-                    # a drop-in replacement
-                    or "is working" in result.get("msg", "")
-                ):
+                if result.get("status") != "ok":
                     return False
 
             except RequestException:
