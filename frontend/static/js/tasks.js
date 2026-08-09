194ae2e096e57316da695a7352010215497abf7f
CasVT <casvantijn@gmail.com>
2026-08-09 22:06:27 +0200

    Added frontend for cron support for scheduled tasks (Resolves #135)

--- frontend/static/js/tasks.js
+++ frontend/static/js/tasks.js
@@ -8,22 +8,102 @@ const TaskEls = {
 	buttons: {
 		refresh: document.querySelector('#refresh-button'),
 		clear: document.querySelector('#clear-button')
+	},
+	edit: {
+		form: document.querySelector("#task-form"),
+		preset: document.querySelector('#task-schedule-preset'),
+		custom: document.querySelector('#custom-schedule'),
+		error: document.querySelector('#schedule-error')
 	}
 };
 
 //
 // Task planning
 //
-function convertInterval(interval) {
-	result = Math.round(interval / 3600); // seconds -> hours
-	return `${result} hours`;
+const scheduleDescriptions = {
+	'0 * * * *': 'Every hour',
+	'0 0 * * *': 'Once per day'
 };
 
 function convertTime(epoch, future) {
-	if (epoch === null) return 'Never';
-	result = Math.round(Math.abs(Date.now() / 1000 - epoch) / 3600); // delta hours
-	if (future) return `in ${result} hours`;
-	else return `${result} hours ago`;
+	if (epoch === null)
+		return 'Never';
+
+	const deltaSeconds = Math.round(Math.abs(Date.now() / 1000 - epoch));
+	const deltaMinutes = Math.round(deltaSeconds / 60);
+	const deltaHours = Math.round(deltaMinutes / 60);
+	const deltaDays = Math.round(deltaHours / 24);
+
+	let unit;
+	let value;
+	if (deltaSeconds == 1) {
+		unit = "second";
+		value = deltaSeconds;
+	}
+	else if (deltaSeconds < 60) {
+		unit = "seconds";
+		value = deltaSeconds;
+	}
+	else if (deltaMinutes == 1) {
+		unit = "minute";
+		value = deltaMinutes;
+	}
+	else if (deltaMinutes < 60) {
+		unit = "minutes";
+		value = deltaMinutes;
+	}
+	else if (deltaHours == 1) {
+		unit = "hour";
+		value = deltaHours;
+	}
+	else if (deltaHours < 24) {
+		unit = "hours";
+		value = deltaHours;
+	}
+	else if (deltaDays == 1) {
+		unit = "day";
+		value = deltaDays;
+	}
+	else {
+		unit = "days"
+		value = deltaDays;
+	}
+
+	if (future)
+		return `in ${value} ${unit}`;
+	else
+		return `${value} ${unit} ago`;
+};
+
+let currentScheduleTask = null;
+
+function openScheduleEditor(taskName, schedule) {
+	currentScheduleTask = taskName;
+	TaskEls.edit.preset.value = Object.keys(scheduleDescriptions).includes(schedule) ? schedule : "";
+	TaskEls.edit.custom.value = schedule;
+	hide([TaskEls.edit.error]);
+	showWindow("task-window");
+};
+
+function submitScheduleEditor(apiKey) {
+	const schedule = TaskEls.edit.custom.value.trim();
+	hide([TaskEls.edit.error]);
+	sendAPI('PUT', '/system/tasks/planning', apiKey, {}, {
+		task_name: currentScheduleTask,
+		schedule: schedule
+	})
+	.then(() => {
+		currentScheduleTask = null;
+		fillPlanning(apiKey);
+		closeWindow();
+	})
+	.catch((e) => {
+		if (e.status === 400) {
+			hide([], [TaskEls.edit.error])
+		}
+		else
+			console.log(e)
+	});
 };
 
 function fillPlanning(api_key) {
@@ -35,14 +115,16 @@ function fillPlanning(api_key) {
 			entry.dataset.task_name = task.task_name;
 
 			entry.querySelector('.name-column').innerText = task.display_name;
-			entry.querySelector('.interval-column').innerText =
-				convertInterval(task.interval);
+			entry.querySelector('.schedule-column').innerText =
+				scheduleDescriptions[task.schedule] || task.schedule;
 			entry.querySelector('.prev-column').innerText =
 				convertTime(task.last_run, false);
 			entry.querySelector('.next-column').innerText =
 				convertTime(task.next_run, true);
-			entry.querySelector('button').onclick =
-				e => sendAPI('POST', '/system/tasks', api_key, {}, {'cmd': task.task_name})
+			entry.querySelector('.actions-column button:first-of-type').onclick =
+				e => sendAPI('POST', '/system/tasks', api_key, {}, {'cmd': task.task_name});
+			entry.querySelector('.actions-column button:last-of-type').onclick =
+				e => openScheduleEditor(task.task_name, task.schedule);
 
 			TaskEls.intervals.appendChild(entry);
 		});
@@ -82,6 +164,22 @@ usingApiKey()
 .then(api_key => {
 	fillHistory(api_key);
 	fillPlanning(api_key);
-	TaskEls.buttons.refresh.onclick = e => fillHistory(api_key);
+	TaskEls.buttons.refresh.onclick = e => {
+		fillPlanning(api_key)
+		fillHistory(api_key)
+	};
 	TaskEls.buttons.clear.onclick = e => clearHistory(api_key);
+
+	TaskEls.edit.preset.onchange = e => {
+		if (e.target.value === '') {
+			TaskEls.edit.custom.value = '';
+		} else {
+			TaskEls.edit.custom.value = e.target.value;
+		}
+	};
+
+	TaskEls.edit.form.onsubmit = (e) => {
+		e.preventDefault();
+		submitScheduleEditor(api_key);
+	};
 });
