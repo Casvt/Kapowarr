0d5270a08f76db5bbce3008e12fa2543f378581d
CasVT <casvantijn@gmail.com>
2026-09-08 18:34:16 +0200

    Don't delete source file if conversion failed
    Co-Authored-By: Silas M Knight <silasfelinus@gmail.com>

--- backend/implementations/converters.py
+++ backend/implementations/converters.py
@@ -9,7 +9,7 @@ from __future__ import annotations
 from functools import lru_cache
 from itertools import chain
 from os import utime
-from os.path import basename, dirname, getmtime, join, splitext
+from os.path import basename, dirname, getmtime, isdir, isfile, join, splitext
 from typing import Dict, List, Set, Tuple, Union
 from zipfile import ZipFile
 
@@ -316,7 +316,7 @@ def zip_to_rar(file: str) -> List[str]:
     with ZipFile(file, 'r') as zip:
         zip.extractall(archive_folder)
 
-    run_rar([
+    result = run_rar([
         'a', # Add files to archive
         '-ep', # Exclude paths from names
         '-inul', # Disable all messages
@@ -324,11 +324,18 @@ def zip_to_rar(file: str) -> List[str]:
         archive_folder # Source folder
     ])
 
+    target_file = splitext(file)[0] + '.rar'
+    if result.returncode != 0 or not isfile(target_file):
+        LOGGER.error(f"Failed to rar up files: Error {result.returncode}")
+        if isfile(target_file):
+            delete_file_folder(target_file)
+        return [file]
+
     delete_file_folder(archive_folder)
     delete_file_folder(file)
     delete_empty_parent_folders(dirname(file), volume_folder)
 
-    return [splitext(file)[0] + '.rar']
+    return [target_file]
 
 
 @ConvertersManager.register_converter("zip", "cbr", supports_32bit=False)
@@ -425,12 +432,18 @@ def rar_to_zip(file: str) -> List[str]:
     archive_folder = generate_archive_folder(volume_folder, file)
     create_folder(archive_folder)
 
-    run_rar([
+    result = run_rar([
         'x', # Extract files with full path
         '-inul', # Disable all messages
         file, # Source archive file
         archive_folder # Target folder to extract into
     ])
+
+    if result.returncode != 0 or not list_files(archive_folder):
+        LOGGER.error(f"Failed to unrar files: Error {result.returncode}")
+        if isdir(archive_folder):
+            delete_file_folder(archive_folder)
+        return [file]
 
     # Files that are put in a ZIP file have to have a minimum last
     # modification time.
